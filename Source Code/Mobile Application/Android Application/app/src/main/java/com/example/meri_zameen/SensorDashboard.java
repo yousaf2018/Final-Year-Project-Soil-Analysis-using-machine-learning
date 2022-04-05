2d07c76cddd06cffbc05e05660800fb7e3926a6e
Your Name <mahmoodyousaf975974@gmail.com>
2022-04-05 16:13:02 +0500

    Sensor dashboard is designed

--- Source Code/Mobile Application/Android Application/app/src/main/java/com/example/meri_zameen/SensorDashboard.java
+++ Source Code/Mobile Application/Android Application/app/src/main/java/com/example/meri_zameen/SensorDashboard.java
@@ -10,6 +10,5 @@ public class SensorDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_dashboard);
-        getSupportActionBar().hide();
     }
 }
